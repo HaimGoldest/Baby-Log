48008a040d3c595b0fb6f565267e6e5495f8bdc1
Haim Goldstein <haim.goldest@gmail.com>
2024-03-01 16:11:54 +0200

    add baby measurements part 3

--- src/app/growth-tracking/growth-tracking.component.ts
+++ src/app/growth-tracking/growth-tracking.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-growth-tracking',
@@ -7,9 +6,4 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrl: './growth-tracking.component.css',
 })
 export class GrowthTrackingComponent {
-  constructor(private router: Router, private route: ActivatedRoute) {}
-
-  navigate() {
-    this.router.navigate(['new'], { relativeTo: this.route });
-  }
 }
