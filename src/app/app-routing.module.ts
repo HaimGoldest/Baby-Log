7b4c05f9128b525dffc1eaff01677ea7c7d05f8c
Haim Goldstein <60044700+HaimGoldest@users.noreply.github.com>
2024-05-10 16:09:21 +0300

    add user authentication (#3)
    * add user auth
    
    * add facebook login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,27 @@ import { RouterModule, Routes } from '@angular/router';
 import { BabyActionsPreferencesComponent } from './baby-actions/baby-actions-preferences/baby-actions-preferences.component';
 import { BabyActionsComponent } from './baby-actions/baby-actions.component';
 import { GrowthTrackingComponent } from './growth-tracking/growth-tracking.component';
+import { AuthGuardService } from './services/auth.guard.service';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', component: BabyActionsComponent },
-  { path: 'baby-actions', component: BabyActionsComponent },
-  { path: 'growth-tracking', component: GrowthTrackingComponent },
-  { path: 'growth-tracking', component: GrowthTrackingComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'baby-actions',
+    component: BabyActionsComponent,
+    canActivate: [AuthGuardService],
+  },
+  {
+    path: 'growth-tracking',
+    component: GrowthTrackingComponent,
+    canActivate: [AuthGuardService],
+  },
   {
     path: 'baby-actions-preferences',
     component: BabyActionsPreferencesComponent,
+
+    canActivate: [AuthGuardService],
   },
 ];
 
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import * as firebaseui from 'firebaseui';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { FacebookAuthProvider, GoogleAuthProvider } from 'firebase/auth';
+import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent implements OnInit, OnDestroy {
+  ui: firebaseui.auth.AuthUI;
+  errorMsg: string = null;
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private router: Router,
+    private userService: UserService
+  ) {}
+
+  ngOnInit() {
+    this.afAuth.app.then((app) => {
+      const uiConfig = {
+        signInOptions: [
+          GoogleAuthProvider.PROVIDER_ID,
+          FacebookAuthProvider.PROVIDER_ID,
+        ],
+        callbacks: {
+          signInSuccessWithAuthResult: this.onLoginSuccessful.bind(this),
+        },
+      };
+
+      this.ui = new firebaseui.auth.AuthUI(app.auth());
+      this.ui.start('#firebaseui-auth-container', uiConfig);
+    });
+  }
+
+  ngOnDestroy() {
+    this.ui.delete();
+  }
+
+  onLoginSuccessful(result) {
+    console.log('onLoginSuccessful');
+    if (result.error) {
+      console.error('Firebase UI error:', result.error);
+    } else {
+      console.log('Firebase UI result:', result);
+    }
+  }
+}
