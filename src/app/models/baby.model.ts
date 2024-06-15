03a316cf8356af6b85b1fedd00ec9414ce9a79d5
Haim Goldstein <haim.goldest@gmail.com>
2024-06-15 16:21:46 +0300

    remove the package guid-typescript from the project

--- src/app/models/baby.model.ts
+++ src/app/models/baby.model.ts
@@ -1,13 +1,3 @@
-import { GuidService } from '../services/guid.service';
-
 export class BabyModel {
-  public guid: string;
-
-  constructor(
-    private guidService: GuidService,
-    public name: string,
-    public birthDate: Date
-  ) {
-    this.guid = guidService.newGuid();
-  }
+  constructor(public name: string, public birthDate: Date) {}
 }
