a10d6d0603a6bdec6acfe5d157210ea81a5a3352
Haim Goldstein <haim.goldstein@philips.com>
2024-08-29 15:28:21 +0300

    part 1

--- src/app/baby-info/baby-info.component.ts
+++ src/app/baby-info/baby-info.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { BabiesService } from '../services/babies.service';
 import { UserService } from '../services/user.service';
 import { Router } from '@angular/router';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-baby-info',
   templateUrl: './baby-info.component.html',
-  styleUrl: './baby-info.component.css',
+  styleUrls: ['./baby-info.component.css'],
 })
 export class BabyInfoComponent {
   uid: string;
@@ -16,7 +18,9 @@ export class BabyInfoComponent {
   constructor(
     private babiesService: BabiesService,
     private userService: UserService,
-    private router: Router
+    private router: Router,
+    private clipboard: Clipboard,
+    private snackBar: MatSnackBar
   ) {
     this.uid = this.babiesService.babyData.value.uid;
     this.name = this.babiesService.babyData.value.name;
@@ -29,6 +33,13 @@ export class BabyInfoComponent {
     if (userDeleted) {
       this.navigateAfterBabyDeletion();
     }
+  }
+
+  public copyUIDToClipboard(): void {
+    this.clipboard.copy(this.uid);
+    this.snackBar.open('UID copied to clipboard', 'Close', {
+      duration: 2000,
+    });
   }
 
   private navigateAfterBabyDeletion() {
