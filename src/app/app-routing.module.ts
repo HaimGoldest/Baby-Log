82ce55c84055b7a264b34d4ed16502bc2d0f1e17
Haim Goldstein <60044700+HaimGoldest@users.noreply.github.com>
2024-09-02 16:30:23 +0300

    fix of navigation bug (#17)
    Co-authored-by: Haim Goldstein <haim.goldstein@philips.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,34 +8,42 @@ import { AddBabyComponent } from './add-baby/add-baby.component';
 import { BabyInfoComponent } from './baby-info/baby-info.component';
 import { GrowthTrackingInfoItemUpdateComponent } from './growth-tracking/growth-tracking-info/growth-tracking-info-item/growth-tracking-info-item-update/growth-tracking-info-item-update.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
+import { AuthGuardService } from './services/AuthGuardService';
 
 const routes: Routes = [
-  { path: '', component: LoadingSpinnerComponent },
+  { path: '', redirectTo: '/loading', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   {
     path: 'baby-actions',
     component: BabyActionsComponent,
+    canActivate: [AuthGuardService],
   },
   {
     path: 'growth-tracking',
     component: GrowthTrackingComponent,
+    canActivate: [AuthGuardService],
   },
   {
     path: 'growth-tracking-update',
     component: GrowthTrackingInfoItemUpdateComponent,
+    canActivate: [AuthGuardService],
   },
   {
     path: 'baby-actions-preferences',
     component: BabyActionsPreferencesComponent,
+    canActivate: [AuthGuardService],
   },
   {
     path: 'add-baby',
     component: AddBabyComponent,
+    canActivate: [AuthGuardService],
   },
   {
     path: 'baby-info',
     component: BabyInfoComponent,
+    canActivate: [AuthGuardService],
   },
+  { path: 'loading', component: LoadingSpinnerComponent },
 ];
 
 @NgModule({
