91805156a228e129c811cea2782ffe2d320aa101
HaimGoldest <haim.goldest@gmail.com>
2025-05-15 04:32:05 +0300

    part 19

--- src/app/features/growth-tracking/components/growth-tracking-form/growth-tracking-form.component.ts
+++ src/app/features/growth-tracking/components/growth-tracking-form/growth-tracking-form.component.ts
@@ -1,58 +1,27 @@
-import { Component, Inject, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  FormBuilder,
-  FormGroup,
-  Validators,
-  ReactiveFormsModule,
-} from '@angular/forms';
-import {
-  MatDialogRef,
-  MAT_DIALOG_DATA,
-  MatDialogModule,
-} from '@angular/material/dialog';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatTimepickerModule } from '@angular/material/timepicker';
 import { MatButtonModule } from '@angular/material/button';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import {
-  DateAdapter,
-  MAT_DATE_FORMATS,
-  MAT_DATE_LOCALE,
-} from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
 import {
-  MatMomentDateModule,
-  MomentDateAdapter,
-  MAT_MOMENT_DATE_ADAPTER_OPTIONS,
-} from '@angular/material-moment-adapter';
+  MatDialogModule,
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { BabyMeasurement } from '../../../../models/baby.model';
-
-export const DATE_FORMATS = {
-  parse: {
-    dateInput: 'DD/MM/YYYY',
-  },
-  display: {
-    dateInput: 'DD/MM/YYYY',
-    monthYearLabel: 'MMM YYYY',
-    dateA11yLabel: 'LL',
-    monthYearA11yLabel: 'MMMM YYYY',
-  },
-};
 
 @Component({
   selector: 'app-growth-tracking-form',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [
-    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
-    {
-      provide: DateAdapter,
-      useClass: MomentDateAdapter,
-      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
-    },
-    { provide: MAT_DATE_FORMATS, useValue: DATE_FORMATS },
-  ],
+
   imports: [
     CommonModule,
     ReactiveFormsModule,
@@ -61,44 +30,37 @@ export const DATE_FORMATS = {
     MatInputModule,
     MatButtonModule,
     MatDatepickerModule,
-    MatMomentDateModule,
+    MatNativeDateModule,
     MatIconModule,
+    MatTimepickerModule,
   ],
   templateUrl: './growth-tracking-form.component.html',
   styleUrls: ['./growth-tracking-form.component.scss'],
 })
 export class GrowthTrackingFormComponent {
-  measurementForm: FormGroup;
+  private fb = inject(FormBuilder);
+  public dialogRef = inject(MatDialogRef<GrowthTrackingFormComponent>);
+  public data = inject<BabyMeasurement | null>(MAT_DIALOG_DATA);
 
-  public constructor(
-    private fb: FormBuilder,
-    public dialogRef: MatDialogRef<GrowthTrackingFormComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: BabyMeasurement | null
-  ) {
-    this.initForm(data);
-  }
+  public measurementForm = this.fb.group({
+    date: [this.data?.date || new Date()],
+    weight: [
+      this.data?.weight || '',
+      [Validators.required, Validators.pattern('^[0-9]+(\\.[0-9]+)?$')],
+    ],
+    height: [
+      this.data?.height || '',
+      [Validators.required, Validators.pattern('^[0-9]+(\\.[0-9]+)?$')],
+    ],
+    headMeasure: [
+      this.data?.headMeasure || '',
+      [Validators.required, Validators.pattern('^[0-9]+(\\.[0-9]+)?$')],
+    ],
+  });
 
   public onSubmit(): void {
     if (this.measurementForm.valid) {
       this.dialogRef.close(this.measurementForm.value);
     }
-  }
-
-  private initForm(data: BabyMeasurement | null): void {
-    this.measurementForm = this.fb.group({
-      date: [data?.date || new Date()],
-      weight: [
-        data?.weight || '',
-        [Validators.required, Validators.pattern('^[0-9]+(\\.[0-9]+)?$')],
-      ],
-      height: [
-        data?.height || '',
-        [Validators.required, Validators.pattern('^[0-9]+(\\.[0-9]+)?$')],
-      ],
-      headMeasure: [
-        data?.headMeasure || '',
-        [Validators.required, Validators.pattern('^[0-9]+(\\.[0-9]+)?$')],
-      ],
-    });
   }
 }
