dc62749abad555267ca83e2d02d6e8257050991e
Haim Goldstein <haim.godest@gmail.com>
2025-06-02 01:18:48 +0300

    refactor: reorganize Firebase imports for clarity

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,12 @@ import {
   LOCALE_ID,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { environment } from '../environments/environment';
 import { appRoutes } from './app.routes';
-import { provideFirebaseApp } from '@angular/fire/app';
-import { provideAuth } from '@angular/fire/auth';
-import { provideFirestore } from '@angular/fire/firestore';
-import { provideStorage } from '@angular/fire/storage';
+import { environment } from '../environments/environment';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
 import { MatButtonModule } from '@angular/material/button';
 import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
@@ -17,10 +17,6 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
 
 export const appConfig: ApplicationConfig = {
   providers: [
