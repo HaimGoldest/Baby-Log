fef08347ee091305040eb98fb0b1c434ec19e2df
Haim Goldstein <haim.goldest@gmail.com>
2026-04-25 02:02:34 +0300

    add firebase emulators

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,14 +1,14 @@
 export const environment = {
-    production: false,
-  
-    firebaseConfig: {
-      apiKey: "AIzaSyBpR7-Vrb4x-Z-jIyJnmi3WVXC3Xf_6iWA",
-      authDomain: "baby-log-management.firebaseapp.com",
-      projectId: "baby-log-management",
-      storageBucket: "baby-log-management.appspot.com",
-      messagingSenderId: "588172152694",
-      appId: "1:588172152694:web:29a4c0c9589cbb7f453457",
-      measurementId: "G-SKVTBYQBWM"
-    },
-  };
-  
+  production: false,
+  useFirebaseEmulators: true,
+
+  firebaseConfig: {
+    apiKey: 'AIzaSyBpR7-Vrb4x-Z-jIyJnmi3WVXC3Xf_6iWA',
+    authDomain: 'baby-log-management.firebaseapp.com',
+    projectId: 'baby-log-management',
+    storageBucket: 'baby-log-management.appspot.com',
+    messagingSenderId: '588172152694',
+    appId: '1:588172152694:web:29a4c0c9589cbb7f453457',
+    measurementId: 'G-SKVTBYQBWM',
+  },
+};
